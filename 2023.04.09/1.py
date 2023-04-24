1d418c1b67c3f43d5870381f13220a72d68fec51
AvilovAlexandr90 <89531495585@mail.ru>
2023-04-24 23:23:17 +0300

    первое дз

---
+++ 2023.04.09/1.py
@@ -0,0 +1,8 @@
+name = input('Введите имя: ')
+lastname = input('Введите фамилию: ')
+age = int(input('Введите год рождения: '))
+old = str(2023 - age)
+
+print(f'{name + " " + lastname + ", " + old}')
+
+#Авилов Александр, 33
